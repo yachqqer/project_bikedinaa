a998b5249e319e5f8fde01341b4278207b1f794b
veronika <xxzvvwaq@gmail.com>
2025-11-29 10:23:58 +0300

    сделанная 6 практическая

--- PZ_6/PZ_6_3.py
+++ PZ_6/PZ_6_3.py
@@ -3,21 +3,27 @@
 
 
 import random
-# Исходный список
+
+
+def replace_maximum(spisok: list[int]) -> list[int]:
+    return_list = spisok.copy()
+    for i in range(1, len(spisok) - 1):
+        if spisok[i-1] < spisok[i] > spisok[i+1]:
+            return_list[i] = 0
+    if spisok[0] > spisok[1]:
+        return_list[0] = 0
+    if spisok[-1] > spisok[-2]:
+        spisok[-1] = 0
+    return return_list
+
+
 try:
-    T = int(input("Введите длину списка: "))
-    A = [random.randint(0, 100) for i in range(T)]
-    N = len(A)
+    n = int(input('Введите размер списка: '))
+except ValueError:
+    print('Введите число')
+    exit()
 
-    print("Исходный список:", A)
+nums = [random.randint(0, 100) for i in range(n)]
 
-# Проходим по элементам, кроме первого и последнего (у них только один сосед)
-    for i in range(1, N-1):
-        if A[i]>A[i-1]:
-            if A[i]>A[i+1]:
-                A[i] = 0
-
-    print("Результат:      ", A)
-
-except ValueError or NameError as e:
-    print(e)
+print(nums)
+print(replace_maximum(nums))
