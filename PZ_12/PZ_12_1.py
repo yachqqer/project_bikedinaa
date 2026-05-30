152e2d8ac0e04006ffdc4deaadf42eacc4d4bda7
veronika <xxzvvwaq@gmail.com>
2026-05-30 19:13:33 +0300

    сделанная 12 практическая

--- PZ_12/PZ_12_1.py
+++ PZ_12/PZ_12_1.py
@@ -1,11 +1,30 @@
 # В матрице элементы второго столбца возвести в квадрат.
 
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+import random
+
+try:
+    elements = int(input('Введите количество элементов в матрице: '))
+except ValueError:
+    print('Введите число.')
+    exit()
+
+def generate_matrix(el: int) -> list[list[int]]:
+    return [
+        [random.randint(1, 10) for _ in range(el)]
+        for _ in range(el)
+    ]
+
+matrix = generate_matrix(elements)
 
 modify_row = lambda r: r[:1] + [r[1] ** 2] + r[2:]
 
-new_matrix = list(map(modify_row, matrix))
+new_matrix = list(map(modify_row, matrix ))
 
-print("Новая матрица:")
-print(new_matrix)
+print("Исходная матрица: ")
+for i in matrix:
+    print(i)
+
+print("Модифицированная матрица: ")
+for i in new_matrix:
+    print(i)
 
